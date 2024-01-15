2db9d5a29919e57471e8a2a863bb54f6b50e8d0a
TristanGeh <109686323+TristanGeh@users.noreply.github.com>
2024-01-15 04:54:25 +0100

    Change: Project's architecture to align with the MVVM Model

---
+++ UserList/Sources/UserListViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  UserListViewModel.swift
+//  UserList
+//
+//  Created by Tristan Géhanne on 15/01/2024.
+//
+
+import Foundation
+
+class UserListViewModel: ObservableObject {
+    // OutPut :
+    // TODO: - Those properties should be viewModel's OutPuts
+    @Published private(set) var users: [User] = []
+    @Published private(set) var isLoading = false
+    @Published  var isGridView = false
+    
+    // TODO: - Should be an OutPut
+     func shouldLoadMoreData(currentItem item: User) -> Bool {
+        guard let lastItem = users.last else { return false }
+        return !isLoading && item.id == lastItem.id
+    }
+    
+    //Input :
+    
+    // TODO: - Should be a viewModel's input
+     func reloadUsers() {
+        users.removeAll()
+        fetchUsers()
+    }
+    // TODO: - Should be a viewModel's input
+     func fetchUsers() {
+        isLoading = true
+        Task {
+            do {
+                let users = try await repository.fetchUsers(quantity: 20)
+                self.users.append(contentsOf: users)
+                isLoading = false
+            } catch {
+                print("Error fetching users: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // TODO: - The property should be declared in the viewModel
+     let repository = UserListRepository()
+}
+
+
+
+
